e65628e17587f15a45c4518d048b4ca63f06d6d7
vickofranic <vfranic@gmail.com>
2016-04-03 12:04:56 +0200

    Vjezba 3

---
+++ Vjezba_3_C_sharp/Triangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+
+namespace Labs
+{
+    /**
+     * class Triangle extends Shape
+     */
+    class Triangle : Shape
+    {
+        /**
+         * @param oneSide
+         */
+        private double oneSide;
+
+        /**
+         * Constructor for Triangle class
+         * 
+         * @param s - side of equilateral triangle
+         */
+        public Triangle(double s)
+        {
+            oneSide = s;
+            xPos = DataModel.getNewXPos();
+            yPos = DataModel.getNewYPos();
+        }
+
+        /**
+         * Returns area for triangle
+         *
+         * @return double
+         */
+        public override double getArea()
+        {
+            return ((Math.Sqrt(3)/4) * (oneSide * oneSide));
+        }
+
+        /**
+         * Returns perimeter of triangle
+         * 
+         * @return double
+         */
+        public override double getPerimeter()
+        {
+            return oneSide * 3;
+        }
+
+        /**
+         * Prints triangle data to the console
+         */
+        public override void printData()
+        {
+            Console.WriteLine();
+            Console.WriteLine("My type: " + this.GetType());
+            Console.WriteLine("Side of triangle = " + oneSide);
+            Console.WriteLine("X position = " + xPos);
+            Console.WriteLine("Y position = " + yPos);
+        }
+    }
+}
