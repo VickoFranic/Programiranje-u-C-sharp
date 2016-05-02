d065c64b68a4eb0b2d0c3754fa51de7acaa38efd
vickofranic <vfranic@gmail.com>
2016-05-02 16:57:01 +0200

    C #

---
+++ Vjezba_6_C_sharp - Copy/PersonAddOrEditForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vjezba_5
+{
+    public partial class PersonAddOrEditForm : Form
+    {
+        public Person newPerson = new Person();
+
+        public PersonAddOrEditForm()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Person person = new Person(textBox1.Text, textBox2.Text, comboBox1.Text, int.Parse(textBox3.Text));
+                newPerson = person;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Vjezba_6_C_sharp - Copy/PersonsListForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Vjezba_5
+{
+    public partial class PersonsListForm : Form
+    {
+        public PersonsListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newPersonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PersonAddOrEditForm addOrEditForm = new PersonAddOrEditForm();
+            addOrEditForm.ShowDialog(this);
+            
+            if (addOrEditForm.DialogResult == DialogResult.OK)
+            {
+                // Get Person model for ListView
+                Person p = addOrEditForm.newPerson;
+
+                // Save new person to repository
+                PersonRepository.savePerson(p);
+
+                // Create ListView item to append
+                ListViewItem item = new ListViewItem(new []{ p.Name, p.LastName });
+                listView1.Items.Add(item);
+
+                addOrEditForm.Dispose();
+            }
+        }
+
+    }
+}
