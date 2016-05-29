a5d0b211bfbd3c699a3f8a730f46531537c95c3f
vickofranic <vfranic@gmail.com>
2016-05-29 18:40:37 +0200

    Entity Framework App update

---
+++ Vjezba_11_C_sharp/Vjezba_11_C_sharp_baze_podataka/AppForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Vjezba_11_C_sharp_baze_podataka
+{
+    partial class AppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(13, 43);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(344, 511);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(490, 43);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(308, 511);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Ime";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Prezime";
+            this.columnHeader3.Width = 150;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "STUDENTI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(611, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PREDMETI";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Id";
+            this.columnHeader4.Width = 35;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Naziv";
+            this.columnHeader5.Width = 266;
+            // 
+            // AppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 566);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "AppForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
+
---
+++ Vjezba_11_C_sharp/Vjezba_11_C_sharp_baze_podataka/AppForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vjezba_11_C_sharp_baze_podataka
+{
+    public partial class AppForm : Form
+    {
+   
+        public AppForm()
+        {
+            InitializeComponent();
+
+            populateForm();
+        }
+
+        protected void populateForm()
+        {
+            StudentiEntities DB = new StudentiEntities();
+
+            var allStudents = from student in DB.Studenti
+                           select student;
+
+            foreach (Studenti student in allStudents)
+            {
+                ListViewItem lvi = new ListViewItem(student.Id.ToString());
+                lvi.SubItems.Add(student.Ime);
+                lvi.SubItems.Add(student.Prezime);
+
+                listView1.Items.Add(lvi);
+            }
+
+            var allCourses = from course in DB.Predmeti
+                             select course;
+
+            foreach (Predmeti course in allCourses)
+            {
+                ListViewItem lvi = new ListViewItem(course.Id.ToString());
+                lvi.SubItems.Add(course.Naziv);
+
+                listView2.Items.Add(lvi);
+            }
+        }
+
+    }
+}
