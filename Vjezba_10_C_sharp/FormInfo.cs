f2c3f6f9b06558f4a0f128ad41747546eb2cd558
vickofranic <vfranic@gmail.com>
2016-05-29 13:14:54 +0200

    Vjezba rjesena

---
+++ Vjezba_10_C_sharp/FormInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Labs
+{
+    class FormInfo
+    {
+        public int width;
+        public int height;
+        public int xPos;
+        public int yPos;
+
+        public FormInfo(int w, int h, int x, int y)
+        {
+            width = w;
+            height = h;
+            xPos = x;
+            yPos = y;
+        }
+
+        /**
+         * Serialize data to JSON string and write to file
+         * using JSON.net package
+         */
+        public void serialize()
+        {
+            string output = JsonConvert.SerializeObject(this);
+            File.WriteAllText(@"./files/formInfo.json", output);
+        }
+    }
+}
